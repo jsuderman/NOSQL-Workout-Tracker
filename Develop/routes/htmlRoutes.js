ead7018250d21304714765c0c535ccf87aa39d3b
Justin Suderman <jsuderman1@gmail.com>
2020-08-28 10:19:03 -0400

    added html routes to render html pages

---
+++ Develop/routes/htmlRoutes.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+modules.export = function(app) {
+    // get route for home or anything besides exercise or stats page
+    app.get("/", function(req, res) { 
+        res.sendFile(path.join(__dirname, "../public/index.htnl"));
+    });
+
+    // get route for exercise html
+    app.get("/exercise", function(req, res) { 
+        res.sendFile(path.join(__dirname, "../public/exercise.htnl"));
+    });
+
+    //get route for stats html
+    app.get("/stats", function(req, res) { 
+        res.sendFile(path.join(__dirname, "../public/stats.htnl"));
+    });
+
+}
